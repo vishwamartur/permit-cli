a4e0d9d4f678add9d300993a779595e5450738af
Gabriel Manor <g.e.manor@gmail.com>
2024-10-27 23:30:07 -0400

    Run Command Improvements

--- source/commands/pdp/run.tsx
+++ source/commands/pdp/run.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { AuthProvider } from '../../components/AuthProvider.js';
 import PDPCommand from '../../components/PDPCommand.js';
+import { type infer as zInfer, number, object } from 'zod';
+import { option } from 'pastel';
 
-export default function Run() {
+export const options = object({
+	opa: number().optional().describe(option({ description: 'Expose OPA port from the PDP' })),
+});
+
+type Props = {
+	options: zInfer<typeof options>;
+};
+
+export default function Run({ options: { opa } }: Props) {
 	return (
 		<AuthProvider>
-			<PDPCommand />
+			<PDPCommand opa={opa} />
 		</AuthProvider>
 	);
 }

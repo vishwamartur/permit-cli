ba8583e176430dacc4f7a1c1872c6300146b503b
Gabriel Manor <g.e.manor@gmail.com>
2024-11-20 20:28:12 +0200

    Fix Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,66 +1,66 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import sonarjsPlugin from "eslint-plugin-sonarjs";
-import reactPlugin from "eslint-plugin-react";
-import reactHooksPlugin from "eslint-plugin-react-hooks";
-import prettierPlugin from "eslint-plugin-prettier";
+import { FlatCompat } from '@eslint/eslintrc';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import sonarjsPlugin from 'eslint-plugin-sonarjs';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import prettierPlugin from 'eslint-plugin-prettier';
 import js from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
 
-
 const compat = new FlatCompat({
-  baseDirectory: import.meta.url,
+	baseDirectory: import.meta.url,
 });
 
-const eslintConfig = [js.configs.recommended,
-...compat.extends('plugin:react/recommended'),
-  prettierConfig
-]
+const eslintConfig = [
+	js.configs.recommended,
+	...compat.extends('plugin:react/recommended'),
+	prettierConfig,
+];
 
 export default [
-  ...eslintConfig,
-  {
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      globals: {
-        Headers: "readonly",
-        RequestInit: "readonly",
-        fetch: "readonly",
-        process: "readonly"
-      },
-    },
-  },
-  {
-    files: ["source/**/*.{js,ts,tsx}", "*/.{js,ts,jsx,tsx}"],
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-      sonarjs: sonarjsPlugin,
-      react: reactPlugin,
-      "react-hooks": reactHooksPlugin,
-      prettier: prettierPlugin,
-    },
-    rules: {
-      ...reactPlugin.configs.recommended.rules,
-      ...reactHooksPlugin.configs.recommended.rules,
-      ...tsPlugin.configs['recommended'].rules,
-      "no-use-before-define": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "sonarjs/no-identical-functions": "error",
-      "sonarjs/no-duplicate-string": "error",
-      'prettier/prettier': 'warn',
-    },
-  },
-  {
-    settings: {
-      react: { version: "detect" },
-    },
-  },
+	...eslintConfig,
+	{
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+			globals: {
+				Headers: 'readonly',
+				RequestInit: 'readonly',
+				fetch: 'readonly',
+				process: 'readonly',
+			},
+		},
+	},
+	{
+		files: ['source/**/*.{js,ts,tsx}', '*/.{js,ts,jsx,tsx}'],
+		plugins: {
+			'@typescript-eslint': tsPlugin,
+			sonarjs: sonarjsPlugin,
+			react: reactPlugin,
+			'react-hooks': reactHooksPlugin,
+			prettier: prettierPlugin,
+		},
+		rules: {
+			...reactPlugin.configs.recommended.rules,
+			...reactHooksPlugin.configs.recommended.rules,
+			...tsPlugin.configs['recommended'].rules,
+			'no-use-before-define': 'warn',
+			'@typescript-eslint/no-unused-vars': 'warn',
+			'sonarjs/no-identical-functions': 'error',
+			'sonarjs/no-duplicate-string': 'error',
+			'prettier/prettier': 'warn',
+		},
+	},
+	{
+		settings: {
+			react: { version: 'detect' },
+		},
+	},
 ];
